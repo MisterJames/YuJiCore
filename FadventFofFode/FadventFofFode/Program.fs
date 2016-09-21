4ec2152f22a2253f1d8c9f5618451df51652cf60
James Chambers <james@jameschambers.com>
2016-09-21 09:07:36 -0500

    version 1

--- FadventFofFode/FadventFofFode/Program.fs
+++ FadventFofFode/FadventFofFode/Program.fs
@@ -1,48 +1,20 @@
-﻿
-//[<EntryPoint>]
-//let main argv = 
-//    printfn "%A" argv
-//    0 // return an integer exit code
+﻿open System.IO
 
-open System.IO
-
-let readLines (filePath:string) = seq {
-    use sr = new System.IO.StreamReader (filePath)
-    while not sr.EndOfStream do
-        yield sr.ReadLine ()
-}
-
+let readLines (filePath:string) = File.ReadAllText(filePath)
 let input = readLines "C:\Code\YuJimmy\FadventFofFode\FadventFofFode\Day1Input.txt"
 
-//let split = input.Split [|','|]
-
-
 // break down a string into chars
-let explode (s:string) =
-    [for c in s -> c]
-
-printfn "%A" input
-
-
-
-// read the input into a string
-// parse the string into an array/list/seq
-// function that takes a seq and yields 1/-1
-// var = sum of the call of the seq to the function
+let explode (s:string) = [for c in s -> c]
+let split = explode input
 
 // let sampleTableOfSquares = [ for i in 0 .. 99 -> (i, i*i) ]
 let directions = 
-    [ for direction in input ->  
-        if direction = ")" then  
+    [| for direction in split ->  
+        if direction = ')' then  
             -1
         else  
-            1 ]
+            1 |]
 
 
-
-
-
-
-
-
+let finalFloor = directions |> Array.sum  //|> printf "%i"
 
