26b307ca545cb1f7c76dfb2f67e7e749da82e346
cakeller43 <cakeller43@gmail.com>
2016-09-22 09:46:28 -0500

    pattern match part 2

--- FadventFofFode/FadventFofFode/Program.fs
+++ FadventFofFode/FadventFofFode/Program.fs
@@ -1,8 +1,30 @@
-﻿open System.IO
-
-let finalFloor = File.ReadAllText "C:\Code\YuJimmy\FadventFofFode\FadventFofFode\Day1Input.txt" 
+﻿
+open System.IO
+let finalFloor = File.ReadAllText "C:\Dev\YuJiCore\FadventFofFode\FadventFofFode\Day1Input.txt" 
                 |> ( fun s -> [| for c in s -> c |] )                   // convert to char array
                 |> Array.map ( fun x -> if x = ')' then -1 else 1 )     // convert chars to 1/-1
                 |> Array.reduce ( fun acc value -> acc + value )        // reduce array to single value
 
 
+
+let rec parse list floor answer = 
+                                  match list with
+                                  | _ when floor = -1 -> answer //stop rec
+                                  | [] -> floor //stop rec
+                                  | hd::tail ->
+                                                match hd with
+                                                | ')' -> parse tail (floor-1) answer+1
+                                                | '(' -> parse tail (floor+1) answer+1 
+                                                | _ -> parse tail floor answer
+let parseStart list = parse list 0 0
+
+let basement = File.ReadAllText "C:\Dev\YuJiCore\FadventFofFode\FadventFofFode\Day1Input.txt" 
+                |> ( fun s -> [ for c in s -> c ] )
+                |> parseStart
+
+
+//                                                if hd = ')' 
+//                                                then 
+//                                                    parse tail floor+1 answer+1
+//                                                else 
+//                                                    parse tail floor-1 answer+1
