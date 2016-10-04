91b62c88644fca2b1aba0024d944a137450ed32a
James Chambers <james@jameschambers.com>
2016-10-04 10:15:39 -0500

    day 2 part 1 solution

---
+++ FadventFofFode/FadventFofFode/Day2.fs
@@ -0,0 +1,17 @@
+﻿
+open System.IO
+
+let dirPath = "C:\\Code\\YuJimmy\\FadventFofFode\\FadventFofFode\\"   // james
+// let dirPath = ""   // corey
+// let dirPath = ""   // yujie
+
+let allArea = File.ReadAllLines (dirPath + "Day2Input.txt" )
+                |> Seq.map ( fun s -> s.Split[|'x'|] |> Seq.map int |> Seq.sort |> Seq.toArray )
+                |> Seq.map ( fun a -> 3 * a.[0] * a.[1] + 2 * a.[1] * a.[2] + 2 * a.[0] * a.[2] )
+                |> Seq.sum
+
+
+
+
+
+
