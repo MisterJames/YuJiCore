bab5da47b885b9cada06089ab7fa0f99e6c23731
cakeller43 <cakeller43@gmail.com>
2016-11-02 09:54:10 -0500

    skeleton day4

--- FadventFofFode/FadventFofFode/Day4.fsx
+++ FadventFofFode/FadventFofFode/Day4.fsx
@@ -1,1 +1,13 @@
 ﻿
+let input = "bgvyzdsv"
+let prefix = "00000"
+
+let md5 = System.Security.Cryptography.MD5.Create()
+
+
+let rec findHash number = 
+                                // append number to key
+                                // convert to hash
+                                // check leading 5 characters
+                                // if true return number
+                                // else recursive call number + 1
