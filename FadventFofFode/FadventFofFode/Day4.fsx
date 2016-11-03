ad3bf9b04ec8311a9cafd08aa30fd52f6a8c6da8
Corey Keller <cakeller43@gmail.com>
2016-11-03 09:09:59 -0500

    wip

--- FadventFofFode/FadventFofFode/Day4.fsx
+++ FadventFofFode/FadventFofFode/Day4.fsx
@@ -1,20 +1,24 @@
-﻿
+﻿open System
+
 let input = "bgvyzdsv"
 let prefix = "00000"
 
 let md5 = System.Security.Cryptography.MD5.Create()
 
 
-let rec findHash number = input + number 
+let rec findHash number = input + number
                           |> System.Text.Encoding.ASCII.GetBytes
                           |> md5.ComputeHash
-                          |> BitConverter.ToString().Replace("-","")
+                          |> (fun x -> BitConverter.ToString(x))
+                          |> (fun x -> x.Replace("-",""))
                           |> Seq.take 5
                           |> (fun s -> match s with 
-                                        | prefix -> number
-                                        | _ -> findHash number + 1)
+                                            | prefix -> number
+                                            | _ -> Int32.Parse(number) + 1
+                                                    |> (fun x -> findHash(x.ToString())))
                                 // append number to key
                                 // convert to hash
                                 // check leading 5 characters
                                 // if true return number
                                 // else recursive call number + 1
+findHash("0")
