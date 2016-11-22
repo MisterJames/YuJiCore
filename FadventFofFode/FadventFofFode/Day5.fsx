8c8cfab5ea0f734ac45b601343fadab405efd1cc
cakeller43 <cakeller43@gmail.com>
2016-11-22 08:56:29 -0600

    initialize day 5

---
+++ FadventFofFode/FadventFofFode/Day5.fsx
@@ -0,0 +1,18 @@
+﻿open System.IO
+let dirPath = "C:\\Dev\\YuJiCore\\FadventFofFode\\FadventFofFode\\"  // corey
+
+let containsThreeVowels set = set
+
+let doubleLetter set = set
+
+let notContainNaughtyStrings set = set
+
+
+
+let run = File.ReadAllLines (dirPath + "Day5Input.txt" )
+                |> containsThreeVowels
+                |> doubleLetter
+                |> notContainNaughtyStrings
+                |> Seq.length
+
+
